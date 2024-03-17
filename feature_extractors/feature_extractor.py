4d5090d7127b138c1c9e5e6dfe80967b9084b221
Anuradha Chopra <anuradhachopra97@gmail.com>
2024-03-17 19:31:55 +0800

    feat(feature_extractor.py): Add method to save extracted features in a file

--- feature_extractors/feature_extractor.py
+++ feature_extractors/feature_extractor.py
@@ -1,4 +1,7 @@
 from enum import Enum
+import numpy as np
+import torch
+import os
 
 class FeatureExtractors(Enum):
 	MOOD_EXTRACTOR = 0
@@ -26,6 +29,33 @@ class FeatureExtractor:
 
 	def extract_features(self):
 		raise NotImplementedError("Subclasses must implement this method")
+
+	def save_extracted_features(self, _dir):
+		_format = self.get_config_value("format", "npy")
+		if not os.path.isdir(_dir):
+			print("[FeatureExtractor][save_extracted_features] Directory doesn't exist " + str(_dir))
+			return False
+		for feature in self.features.keys():
+			if _format == "npy":
+				path = _dir + "/" + str(self.tag_type) + "_" + str(feature) + ".npy"
+				if isinstance(self.features[feature], np.ndarray):
+					np.save(path, self.features[feature])
+					return True
+				else:
+					np.save(path, self.features[feature].numpy())
+					return True
+			elif _format == "pt":
+				path = _dir + "/" + str(self.tag_type) + "_" + str(feature) + ".pt"
+				torch.save(self.features[feature], path)
+				return True
+			elif _format == "h5py":
+				path = _dir + "/" + str(self.tag_type) + "_" + str(feature) + ".h5"
+				with h5py.File(path, 'w') as hf:
+				    hf.create_dataset(str(feature) + "_dataset", data=tensor.numpy())
+				return True
+			else:
+				print("[FeatureExtractor][save_extracted_features] Invalid format " + str(_format))
+				return False
 
 	def set_source(self, source):
 		if source not in ["raw", "vocals", "drums", "bass", "other"]:
