00ecad8cae1d230a450a41a99753b3d8759b6a8a
Anuradha Chopra <anuradhachopra97@gmail.com>
2024-03-22 12:27:15 +0800

    feat(display_tags.py): Modify tags visualiser to display the features instead of captions. Allow adding root media directory instead of expecting absolute path

--- display_tags.py
+++ display_tags.py
@@ -5,11 +5,13 @@ from pygame import mixer
 import json
 
 class MusicPlayerApp:
-    def __init__(self, root, caption_file_path):
+    def __init__(self, root, caption_file_path, media_root_directory):
         self.root = root
         self.root.title("Music Player")
 
         self.json_file_path = caption_file_path
+
+        self.media_root_directory = media_root_directory
 
         self.file_paths = []
         self.current_index = 0
@@ -35,7 +37,8 @@ class MusicPlayerApp:
         with open(self.json_file_path, 'r') as json_file:
             for line in json_file:
                 item = json.loads(line)
-                self.file_paths.append(item["location"])
+                media_path = self.media_root_directory + item["location"]
+                self.file_paths.append(media_path)
 
         if self.file_paths:
             self.play_current()
@@ -64,15 +67,36 @@ class MusicPlayerApp:
             for i, line in enumerate(json_file):
                 if i == self.current_index:
                     item = json.loads(line)
-                    caption = item["caption"]
+                    # caption = item["caption"]
+                    caption = ""
+                    for key in item.keys():
+                        if not key in ["caption", "location"]:
+                            if key == "beats":
+                                caption = caption + "bpm : " + str(item[key]["bpm"]) + "\n"
+                            elif key == "gender":
+                                if item["voice"] != "instrumental":
+                                    caption = caption + str(key) + " : " + str(item[key]) + "\n"
+                                else:
+                                    continue
+                            elif key == "chords":
+                                caption = caption + str(key) + " "
+                                for i in range(len(item[key])):
+                                    caption = caption + str(item[key][i].strip("\n"))
+                                    if i != len(item[key]) - 1:
+                                        caption = caption + ", "
+                                    if i%4 == 0 and i >= 4:
+                                        caption = caption + "\n"
+                            else:
+                                caption = caption + str(key) + " : " + str(item[key]) + "\n"
                     self.label_caption.config(text=caption)
                     break
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Json with generated captions")
     parser.add_argument("caption_file", help="Path to the file with path and captions")
+    parser.add_argument("media_root_directory", default="", help="Path to root folder of media files")
     args = parser.parse_args()
 
     root = tk.Tk()
-    app = MusicPlayerApp(root, args.caption_file)
+    app = MusicPlayerApp(root, args.caption_file, args.media_root_directory)
     root.mainloop()
